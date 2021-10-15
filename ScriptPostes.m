7099aa222884f0b0d2344627275a8348c7895927
bryan9876 <91569580+bryan9876@users.noreply.github.com>
2021-10-14 23:02:23 -0500

    Update ScriptPostes.m
    Cambios Bryan

--- ScriptPostes.m
+++ ScriptPostes.m
@@ -9,20 +9,28 @@
 
 % limpiar variable 
  clear 
+
 % Minimo de cable
 % Rango de -20.. 20 en i=1
  h= -20:20;
+
 % Valor de la funcion 
  Fh= @(h) sqrt(10^2 + h.^2) + sqrt(15^2 + (20 - h)^2);
+
 % Funcion a plotear  
  y= sqrt(10^2 + h.^2) + sqrt(15^2 - 40*h + h.^2);
+
 % Funcion para determinar valor minimo
  rest= fminbnd(Fh,-20,20);
+
 % Dibujar h,y 
  plot (h,y);
+
 % Titulo 
  title(['Minimo cable usado = ' num2str(rest)]);
+
 % Etiqueta para x
  xlabel(['Min x= ' num2str(rest)]);
+
 % Etiqueta para y 
  ylabel(['Min y= ' num2str( Fh(rest) )]); 
